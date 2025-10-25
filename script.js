1e45f94275dda5d348b5d372386d52fcd7cceee6
rupadevii <rupadevi7094@gmail.com>
2025-10-25 14:50:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+const squares = document.querySelectorAll('.square')
+squares.forEach(square => {
+	square.addEventListener("mouseover", () => {
+		squares.forEach(item => {
+			if(item !== square){
+				item.style.backgroundColor = "brown"	
+			}
+		})
+	})
+	square.addEventListener("mouseout", () => {
+		squares.forEach(item => item.style.backgroundColor = "#E6E6FA")
+	})
+})
+
+// square1.addEventListener("mouseover", () => {
+// 	square2.style.backgroundColor = "brown";
+// 	square3.style.backgroundColor = "brown";
+// }, )
+
+// square1.addEventListener("mouseout", () => {
+// 	square2.style.backgroundColor = "white";
+// 	square3.style.backgroundColor = "white";
+// })
+
